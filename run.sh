3cb2fc1f459e7d236e1c5e615e59e2e11f8d6b16
f2s <dmccue@gmail.com>
2014-11-27 15:29:03 +0100

    run additions

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo docker rm -f wordpress nginx-proxy data-mysql
+sudo docker rm -f wordpress nginx-proxy data-mysql &>/dev/null
 
 #sudo docker pull jwilder/nginx-proxy
 #sudo docker pull tutum/wordpress
-#sudo docker pull mysql
+
+VIRTUAL_HOST=wordpressexample.home24.com
 
 sudo docker run --name data-mysql -v $PWD/data/mysql:/var/lib/mysql scratch bash &>/dev/null
 
 sudo docker run -d --name=nginx-proxy -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
+sudo docker run -d --name=wordpress -e VIRTUAL_HOST=${VIRTUAL_HOST} --volumes-from data-mysql -t tutum/wordpress
 
-sudo docker run -d --name=wordpress -e VIRTUAL_HOST=wordpressexample.home24.com --volumes-from data-mysql -t tutum/wordpress
-
-
+echo Please access via http://${VIRTUAL_HOST}
